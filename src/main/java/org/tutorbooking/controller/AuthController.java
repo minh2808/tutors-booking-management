86175e3b2a0c5705c8e4e4ae55d165ed975f6cd6
Pham Ngoc minh <minhphamngoc808@gmail.com>
2026-03-26 19:52:30 +0700

    feat (oauth2) : test git 2

--- src/main/java/org/tutorbooking/controller/AuthController.java
+++ src/main/java/org/tutorbooking/controller/AuthController.java
@@ -35,7 +35,6 @@ public class AuthController {
         return ResponseEntity.ok(ApiResponse.success("Đăng nhập thành công!", authResponse));
     }
 
-
     @PostMapping("/google/login")
     public ResponseEntity<ApiResponse<AuthResponse>> googleAuthenticateUser(@Valid @RequestBody GoogleLoginRequest googleLoginRequest) {
         AuthResponse authResponse = authService.googleLogin(googleLoginRequest);
