f41a2bcdd2bfe495908e217e69ca6b34219124af
Pham Ngoc minh <minhphamngoc808@gmail.com>
2026-04-02 18:39:20 +0700

    feat (booking) : dto and resource notfound exception

---
+++ src/main/java/org/tutorbooking/dto/response/BookingResponse.java
@@ -0,0 +1,32 @@
+package org.tutorbooking.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+import org.tutorbooking.domain.enums.BookingStatus;
+import org.tutorbooking.domain.enums.TeachingMode;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+@Builder
+public class BookingResponse {
+    private Long id;
+    private Long parentId;
+    private Long tutorId;
+    private Long subjectId;
+    private Long studentId;
+    private Byte gradeLevel;
+    private BigDecimal pricePerSession;
+    private TeachingMode teachingMode;
+    private Boolean isRecurring;
+    private Integer dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate recurringStartDate;
+    private LocalDate recurringEndDate;
+    private BookingStatus status;
+    private List<SessionResponse> sessions;
+}
