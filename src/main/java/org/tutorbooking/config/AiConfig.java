1ab18fdcc6b6446b013e29ec4b73d5e7dd78b0a6
Pham Ngoc minh <minhphamngoc808@gmail.com>
2026-04-11 12:56:52 +0700

    feat(AI) : AI chat bot

--- src/main/java/org/tutorbooking/config/AiConfig.java
+++ src/main/java/org/tutorbooking/config/AiConfig.java
@@ -3,14 +3,14 @@ package org.tutorbooking.config;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAsync
-@ConditionalOnExpression("!'${spring.ai.openai.api-key:}'.isEmpty()")
+
 public class AiConfig {
 
     @Bean
